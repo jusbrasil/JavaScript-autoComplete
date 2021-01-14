500b826da3fc0c12e606fe0c6683704274a53d0e
agnarok <pretogildo@gmail.com>
2021-01-14 16:57:34 -0400

    Adding more test cases

--- tests/utils/string.js
+++ tests/utils/string.js
@@ -18,15 +18,17 @@ describe('String related functions', function () {
         // GIVEN
         var string1 = '<b>test</b>ing';
         var string2 = '<span><span>testing';
+        var string3 = '<span><b>Nested</b> test</span>'
 
         // WHEN
         string1 = removeHtlmTags(string1);
         string2 = removeHtlmTags(string2);
-
+        string3 = removeHtlmTags(string3);
 
         // THEN
         expect(string1).toEqual('testing');
         expect(string2).toEqual('testing');
+        expect(string3).toEqual('Nested test');
 
     })
 })
